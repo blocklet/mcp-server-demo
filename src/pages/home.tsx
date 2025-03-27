dcdcdb2f96320506d2fec5acb8e07b16dfe64142
wangshijun <wangshijun2010@gmail.com>
2025-03-27 15:46:04 +0800

    chore: update home pages

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,48 +1,37 @@
 import { useState } from 'react';
 
 import blockletLogo from '../assets/blocklet.svg';
-import reactLogo from '../assets/react.svg';
-import viteLogo from '../assets/vite.svg';
-import api from '../libs/api';
+import mcpLogo from '../assets/mcp.png';
 import './home.css';
 
 function Home() {
   const [count, setCount] = useState(0);
 
-  async function getApiData() {
-    const { data } = await api.get('/api/data');
-    const { message } = data;
-    alert(`Message from api: ${message}`);
-  }
-
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank" rel="noreferrer">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+        <a href="https://www.modelcontextprotocol.io" target="_blank" rel="noreferrer">
+          <img src={mcpLogo} className="logo" alt="MCP logo" />
         </a>
         <a href="https://www.arcblock.io/docs/blocklet-developer/getting-started" target="_blank" rel="noreferrer">
           <img src={blockletLogo} className="logo blocklet" alt="Blocklet logo" />
         </a>
       </div>
-      <h1>Vite + React + Blocklet</h1>
+      <h1>Model Context Protocol + Blocklet</h1>
       <div className="card">
+        <a type="button" href="/.well-known/service/mcp/servers" target="_blank" rel="noreferrer">
+          Get MCP Server List
+        </a>
+        <br />
+        <br />
         <button type="button" onClick={() => setCount((currentCount) => currentCount + 1)}>
           count is {count}
-        </button>
-        <br />
-        <br />
-        <button type="button" onClick={getApiData}>
-          Get API Data
         </button>
         <p>
           Edit <code>src/app.tsx</code> and save to test HMR
         </p>
       </div>
-      <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
+      <p className="read-the-docs">Click on the MCP and Blocklet logos to learn more</p>
     </>
   );
 }
